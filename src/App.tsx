90f766227e546d9dc0f57e13a5741d2ec4dd2797
Sh3lby007 <zhengshaobin@ymail.com>
2023-02-26 22:15:19 +0800

    Import relevant components into main App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@ import { useState } from "react";
 import "./App.css";
 import Feed from "./components/Feed/Feed";
 import Header from "./components/Header/header";
+import Login from "./components/Login/Login";
 import SideBar from "./components/SideBar/Sidebar";
+import Widgets from "./components/Widgets/Widgets";
 
 function App() {
   const [count, setCount] = useState(0);
+  const user = "";
 
-  return (
-    <div className="app">
-      {/* Header */}
-      <Header />
+  // If no user, show the login page
+  if (!user) return <Login />;
+  // Show app when user is present.
+  else
+    return (
+      <div className="app">
+        {/* Header */}
+        <Header />
 
-      {/* App Body */}
-      <div className="app_body">
-        {/* Sidebar */}
-        <SideBar />
-        {/* Feed */}
-        <Feed />
+        {/* App Body */}
+        <div className="app_body">
+          {/* Sidebar */}
+          <SideBar />
+          {/* Feed */}
+          <Feed />
+          {/* Widgets */}
+          <Widgets />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
